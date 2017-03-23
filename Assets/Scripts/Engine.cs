8f3e4532cb065f14fb37d8d4fdf2ef01891bb193
Ashugeo <hugo.saintemarie@gmail.com>
2017-03-23 16:52:58 +0100

    First commit
    Working physic on drops and collision trigger

---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour {
+
+	public GameObject Drop;
+	public float dropDelay = 1000;
+
+	public float height = 1;
+
+	public GameObject TopLeft;
+	public GameObject TopRight;
+	public GameObject BottomLeft;
+	public GameObject BottomRight;
+
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		//var ambiance = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (startTime + dropDelay < Time.time) {
+			startTime = Time.time;
+			var drop = Instantiate(Drop);
+			drop.transform.position = new Vector3(Random.Range(TopLeft.transform.position.x, BottomLeft.transform.position.x), height, Random.Range(TopLeft.transform.position.z, TopRight.transform.position.z));
+		}
+	}
+}
