0b07a8664a16ee81b2afcbaabb8a455a38315ff6
DESKTOP-BGT713S\Tie Striker <abourgeois27380@gmail.com>
2017-03-28 17:40:56 +0200

    Added xml reader

---
+++ Assets/Scripts/XML/Partition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("Partition")]
+public class Partition {
+
+    public int BPM;
+
+    [XmlArray("Ambiances")]
+    [XmlArrayItem("Ambiance")]
+    public List<Ambiance> Families = new List<Ambiance>();
+
+    public static Partition Load(string path)
+    {
+        XmlRootAttribute xRoot = new XmlRootAttribute();
+        xRoot.ElementName = "Partition";
+        xRoot.IsNullable = true;
+
+        var serializer = new XmlSerializer(typeof(Partition), xRoot);
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            return serializer.Deserialize(stream) as Partition;
+        }
+    }
+}
---
+++ Assets/Scripts/XMLReader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+public class XMLReader : MonoBehaviour
+{
+    public Partition Partition;
+
+    void Start() {
+        Partition = Partition.Load(Path.Combine(Application.dataPath, "Resources/config.xml"));
+        Debug.Log("XML read !");
+        //Debug.Log("Nb famille : " + Partition.Families.Count);
+        //foreach (var family in Partition.Families)
+        //    foreach (var track in family.Tracks)
+        //        Debug.Log("Track rythm : " + track.Rythm);
+    }
+}
